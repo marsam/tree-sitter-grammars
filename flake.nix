099a02e806abf455b8007c85795f5a1abce236f0
Mario Rodas <marsam@users.noreply.github.com>
2025-02-08 09:03:00 -0500

    Generate table with the available grammars

--- flake.nix
+++ flake.nix
@@ -24,6 +24,37 @@
         ci = pkgs.mkShellNoCC {
           packages = [ pkgs.nix-eval-jobs ];
         };
+        readme =
+          let
+            inherit (pkgs.lib) hasPrefix mapAttrsToList optionalString removePrefix;
+            grammars = pkgs.callPackage ./lib/grammars.nix { };
+            drvUrl = drv: "${drv.meta.homepage}/${(if hasPrefix "https://codeberg.org/" drv.src.url then "src" else "tree")}/${drv.src.rev}" + optionalString (drv.location != null) "/${drv.location}";
+            rows = mapAttrsToList (name: drv: "| ${name} | [${removePrefix "unstable-" drv.version}](${drvUrl drv}) |") grammars.grammars;
+            table = pkgs.writeTextFile {
+              name = "table";
+              text = ''
+                <details>
+                  <summary><strong>Available grammars (${toString (builtins.length rows)})</strong></summary>
+
+                | Grammar   | Version |
+                | --------- | ------- |
+                ${builtins.concatStringsSep "\n" rows}
+
+                </details>
+              '';
+            };
+            update-readme = pkgs.writeShellApplication {
+              name = "update-readme";
+              text = ''
+                lead='^<!-- start-gramars -->$'
+                tail='^<!-- end-gramars -->$'
+                sed -i -e "/$lead/,/$tail/{ /$lead/{p; r ${table}" -e "}; /$tail/p; d }" "''${1-README.md}"
+              '';
+            };
+          in
+          pkgs.mkShellNoCC {
+            packages = [ update-readme ];
+          };
       });
 
       packages = forAllSystems (
